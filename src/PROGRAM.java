91f4f0a1539baf299af4c182df31e8fdbef228e0
EMMANUEL <Hi @EmmanuelRB!>
2017-09-21 00:40:46 -0500

    Factorial con funcion recursiva y con ciclo

---
+++ src/PROGRAM.java
@@ -0,0 +1,11 @@
+
+ 
+public class PROGRAM {
+    
+    public static void main (String[] args) {
+        FACTORIAL factorial = new FACTORIAL();
+        //System.out.println(factorial.factorial_iterativo(4));
+        System.out.println(factorial.factorial_recursividad(3));
+    }
+    
+}
